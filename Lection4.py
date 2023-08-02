d865f7cc24a72fec2e585f33d919cbd194035319
Olga_Migal <Tashaloriya@gmail.com>
2023-08-02 20:05:21 +0300

    Лекция №4. Функции высшего порядка (lambda, map, zip, enumerate e.t.c.). Работа с файлами

--- Lection4.py
+++ Lection4.py
@@ -1,10 +1,10 @@
-
+# В списке хранятся числа. Нужно выбрать только чётные числа и составить список пар (число, квадрат числа)
 
 # my_list = [1, 2, 3, 5, 8, 15, 23, 38]
 # answer = [(i, i**2) for i in my_list if i%2==0]
 # print(answer)
 
-# вариант №2
+# вариант №2:
 # res = list()
 #
 # for i in my_list:
@@ -12,17 +12,25 @@
 #         res.append((i, i**2))
 # print(res)
 
- # то же самое через lambda:
-def select(f, col):
-     return [f(x) for x in col]
+ # вариант №3 - то же самое через lambda:
+# def select(f, col):
+#      return [f(x) for x in col]
+#
+# def where(f, col):
+#      return [x for x in col if f(x)]
+#
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# res = select(int, data)
+# print(res)
+# res = where(lambda x: x % 2 == 0, res)
+# print(res)
+# res = list(select(lambda x: (x, x ** 2), res))
+# print(res)
 
-def where(f, col):
-     return [x for x in col if f(x)]
+# Функция map:
 
-data = [1, 2, 3, 5, 8, 15, 23, 38]
-res = select(int, data)
-print(res)
-res = where(lambda x: x % 2 == 0, res)
-print(res)
-res = list(select(lambda x: (x, x ** 2), res))
-print(res)
+list_1 = [x for x in range(1, 20)]
+print(list_1)
+
+list_1 = list(map(lambda x: x+10, list_1))
+print(list_1)
